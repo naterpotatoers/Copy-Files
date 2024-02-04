5c3530967d2972eda784743c4d2afd0df5d5c369
naterpotatoers <nate.garza@hotmail.com>
2024-02-03 22:05:02 -0800

    renames copy file func

--- copyFileSpecifiedNumberOfTimes.bat
+++ copyFileSpecifiedNumberOfTimes.bat
@@ -1,17 +1,17 @@
 @echo off
 @REM This script will copy the specified files a specified number of times.
 
-call:copyFileSpecifiedNumberOfTimes "1915i" 2
-call:copyFileSpecifiedNumberOfTimes "1915i SIR" 2
-call:copyFileSpecifiedNumberOfTimes "MW" 2
-call:copyFileSpecifiedNumberOfTimes "NE" 2
-call:copyFileSpecifiedNumberOfTimes "NE SD" 2
-call:copyFileSpecifiedNumberOfTimes "SDP" 2
-call:copyFileSpecifiedNumberOfTimes "SIR" 2
-call:copyFileSpecifiedNumberOfTimes "TERM" 2
+call:copyFileNumberOfTimes "1915i" 2
+call:copyFileNumberOfTimes "1915i_SIR" 2
+call:copyFileNumberOfTimes "MW" 2
+call:copyFileNumberOfTimes "NE" 2
+call:copyFileNumberOfTimes "NE_SD" 2
+call:copyFileNumberOfTimes "SDP" 2
+call:copyFileNumberOfTimes "SIR" 2
+call:copyFileNumberOfTimes "TERM" 2
 goto:eof
 
-:copyFileSpecifiedNumberOfTimes
+:copyFileNumberOfTimes
 SET "ORIGINAL_FILE_NAME=%~1-0-Audit_Email_2024.docx"
 
 for /l %%i in (1,1,%~2) do (
