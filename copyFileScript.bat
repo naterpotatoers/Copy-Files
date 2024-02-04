b9fa0ead8af3bbeaec1a25ed5fde6c889a3f8a79
naterpotatoers <nate.garza@hotmail.com>
2024-02-03 23:03:09 -0800

    :recycle: adds template name to file data, renames template files, adds year variable

--- copyFileScript.bat
+++ copyFileScript.bat
@@ -1,18 +1,19 @@
 @echo off
 @REM This script will copy the following template files a specified number of times.
 
+SET "year=2024"
 call:copyTemplateFileNumberOfTimes "1915i" 2
-call:copyTemplateFileNumberOfTimes "1915i_SIR" 2
+call:copyTemplateFileNumberOfTimes "1915i-SIR" 2
 call:copyTemplateFileNumberOfTimes "MW" 2
 call:copyTemplateFileNumberOfTimes "NE" 2
-call:copyTemplateFileNumberOfTimes "NE_SD" 2
+call:copyTemplateFileNumberOfTimes "NE-SD" 2
 call:copyTemplateFileNumberOfTimes "SDP" 2
 call:copyTemplateFileNumberOfTimes "SIR" 2
 call:copyTemplateFileNumberOfTimes "TERM" 2
 goto:eof
 
 :copyTemplateFileNumberOfTimes <filePrefix> <numberOfTimesToCopy>
-SET "ORIGINAL_FILE_NAME=2024-Audit-Email-Template-%~1.docx"
+SET "ORIGINAL_FILE_NAME=Audit-Email-Template-%~1.docx"
 
 if not exist "%ORIGINAL_FILE_NAME%" (
     echo "Cannot find %ORIGINAL_FILE_NAME%"
@@ -20,8 +21,8 @@ if not exist "%ORIGINAL_FILE_NAME%" (
 )
 
 for /l %%i in (1,1,%~2) do (
-    if not exist "2024-Audit-Email-%~1-%%i.docx" (
-        copy "%ORIGINAL_FILE_NAME%" "2024-Audit-Email-%~1-%%i.docx"
+    if not exist "%year%-Audit-Email-%~1-%%i.docx" (
+        copy "%ORIGINAL_FILE_NAME%" "%year%-Audit-Email-%~1-%%i.docx"
     )
 )
 goto:eof
